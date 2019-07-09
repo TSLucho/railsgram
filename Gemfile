0e7d08104ed8d47abc4d11aee3f5b280091a81c4
LuisR <luisrodriguezrc94@gmail.com>
2019-07-09 11:16:54 -0400

    Navbar Agregado

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
+#Jquery-Rails
+gem 'jquery-rails', '~> 4.3', '>= 4.3.5'
 #Bootstrap 4.3.1
 gem 'bootstrap', '~> 4.3', '>= 4.3.1'
 #Carrierwave 1.3.1
---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,34 @@
+class ImagesController < ApplicationController
+    def index
+    
+    end
+
+    def new
+    
+    end
+    
+    def create
+    
+    end
+    
+    def show
+    
+    end
+
+    def edit
+    
+    end
+
+    def update
+    
+    end
+    
+    def destroy
+    
+    end
+
+    private
+        def image_params
+            
+        end
+end
